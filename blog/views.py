89456f0d92c14f5f1cdf3d24ed919251867ce9f3
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2023-08-29 08:51:21 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,16 @@ import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+
 from blog.models import Post
 from blog.forms import CommentForm
 
 logger = logging.getLogger(__name__)
 
-
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
-  logger.debug("Got %d posts", len(posts))
-  return render(request, "blog/index.html", {"posts": posts})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 
 def post_detail(request, slug):
